a46409ade65de2e06354509d8028d60bc6a0bfaf
Tyler Burnam <tyler@burnamtech.com>
2020-09-10 09:21:56 -0500

    webapp complete jobDetails types

--- src/jobDetails.ts
+++ src/jobDetails.ts
@@ -1,44 +1,74 @@
-
 export type JobDetailsCommon = {
   numOfSons: number;
 };
 
-type MoveType = 'newLocation' | 'inHome' | 'truckLoading' | 'packing';
+type MoveType = "newLocation" | "inHome" | "truckLoading" | "packing";
 
 export type JobDetailsMoving = JobDetailsCommon & {
+  estimatedMilesDriven?: number;
   distanceBetweenLocations?: number;
   estimatedHours: number;
   needsFurnitureAssembly?: boolean;
   needsPacking?: boolean;
-  needsUhaul?: boolean;
+  needsUhaul?: number;
   numOfFloors?: boolean;
   numOfRooms: number;
   typeOfMove: MoveType;
 };
-
-export type EstimateDetailsMoving = Pick<JobDetailsMoving,
-  "estimatedHours"
->;
 
 export type JobDetailsLandscaping = JobDetailsCommon & {
   estimatedHours: number;
   needsTools: boolean;
   needsTruck: boolean;
   distanceFromLandfill?: number;
-  landscapingType: 'digging' | 'gardening';
 };
-
-export type EstimateDetailsLandscaping = Pick<JobDetailsLandscaping,
-  "estimatedHours" |
-  "needsTools"
->;
 
 export type JobDetailsInteriorPainting = JobDetailsCommon & {
   numOfRooms: number;
+  jobPrepLevel: number;
   numOfWallSections: number;
+  numOfCeilings: number;
+  numOfTrimOrCrown: number;
+  numOfWindows: number;
+  numOfClosets: number;
+  numOfDoors: number;
+  needsTools: boolean;
+  needsPaint: boolean;
 };
 
-export type JobDetails =
-  Partial<JobDetailsMoving> &
-  Partial<JobDetailsLandscaping>
-;
+export type JobDetailsExteriorPainting = JobDetailsCommon & {
+  lengthOfHome: number;
+  widthOfHome: number;
+  numOfStories: number;
+  isThereMoldOrMildew: boolean;
+  levelOfFlakingPaint: string;
+  isThereWoodToBeReplaced: boolean;
+  linearFeetOfTrim: number;
+  numOfWindows: number;
+  numOfWindowUnits: number;
+  numOfDoors: number;
+  numOfGarageDoors: number;
+  linearFeetOfRailing: number;
+  howMuchPrepRequired: string;
+  squareFootageOfTheDeck: number;
+  doesWoodNeedReplacing: boolean;
+};
+
+export type JobDetailsCleaningAndJunk = JobDetailsCommon & {
+  numOfHours: number;
+  needsUhaul: number;
+  distanceFromLandfill: number;
+};
+
+export type JobDetailsSonAssistance = JobDetailsCommon & {
+  numOfHours: number;
+  jobType: string;
+  toolsNeeded: string;
+};
+
+export type JobDetails = Partial<JobDetailsMoving> &
+  Partial<JobDetailsLandscaping> &
+  Partial<JobDetailsInteriorPainting> &
+  Partial<JobDetailsExteriorPainting> &
+  Partial<JobDetailsCleaningAndJunk> &
+  Partial<JobDetailsSonAssistance>;
